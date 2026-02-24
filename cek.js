8268c924a9c6aef3044c71bb8828b89e1d0cf377
utama1995 <redminotee5190@gmail.com>
2026-02-24 14:58:11 +0700

    Update cek.js

--- cek.js
+++ cek.js
@@ -102,19 +102,22 @@ hasil.innerHTML = `
 <div class="label">Nama Aset:</div>
 <div class="value">${data.aset || "-"}</div>
 
+<div class="label">Kategori Aset:</div>
+<div class="value">${found.aset}</div>
+
 <div class="label">Status:</div>
 <div class="value ${statusClass}">
 ${statusText}
 </div>
-
-<div class="label">Catatan GA:</div>
-<div class="value">${data.catatan || "-"}</div>
 
 <div class="label">Vendor:</div>
 <div class="value">${data.vendor || "-"}</div>
 
 <div class="label">Estimasi Selesai:</div>
 <div class="value">${data.estimasi || "-"}</div>
+
+<div class="label">Foto Kerusakan:</div>
+<img src="${found.foto}">
 `;
 
 }catch(err){
