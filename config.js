52f311bd025325a075638bb883a16b26076d669b
utama1995 <redminotee5190@gmail.com>
2026-02-28 10:02:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw-eisVH7f3SaKLzFdNS36ihLPqZj4XWVE_ApBjMPnYA3L1dhwy5MzPEvIFa-Vy7A5Whg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzd5g_vAAFOBCKdeAEmyBy58viBTwcecqntE2QCmtF_FO4lAA23r4u_D_VWCexeMeU-ew/exec";
 
 const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/db00zaan0/image/upload";
 
